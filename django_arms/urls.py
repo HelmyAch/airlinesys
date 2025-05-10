8354a43b8d57bd607a5a724090f2998e0378d27c
helmy <helmyachour@gmail.com>
2025-05-10 23:20:56 +0100

    Ajout des métriques Prometheus

--- django_arms/urls.py
+++ django_arms/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('django_prometheus.urls')),  # <--- AJOUTÉ ICI
     path('', include('armsApp.urls')),
 ]
