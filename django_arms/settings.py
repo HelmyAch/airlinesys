b12c3d4595d6c31150db82ce5e1a792c971dbf61
helmy <helmyachour@gmail.com>
2025-05-10 23:21:23 +0100

    Ajout des métriques Prometheus

--- django_arms/settings.py
+++ django_arms/settings.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'armsApp.apps.armsAppConfig',
     'qr_code',
+    'django_prometheus',
+
 ]
 
 MIDDLEWARE = [
+    'django_prometheus.middleware.PrometheusBeforeMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_prometheus.middleware.PrometheusBeforeMiddleware',
+
 ]
 
 ROOT_URLCONF = 'django_arms.urls'
