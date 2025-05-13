02b17367dda2107b53eb5ef4ed4ab8b336e270b2
helmy <helmyachour@gmail.com>
2025-05-13 01:32:41 +0100

    ok

--- django_arms/settings.py
+++ django_arms/settings.py
@@ -25,19 +25,11 @@ SECRET_KEY = 'django-insecure-j7xv%)66l8u0^8sxnj7()z$zll8&a2*_h2r84yeo9f2fidx8^7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-<<<<<<< HEAD
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '172.19.0.2', 'tunisair-web']
-=======
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'web','tunisair-web-1']
->>>>>>> 9efe092ddad2fe2f3722e48457a65a6b0ad9289b
 
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '172.19.0.2', 'tunisair-web']
 
 # Application definition
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 9efe092ddad2fe2f3722e48457a65a6b0ad9289b
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -53,11 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-<<<<<<< HEAD
-    'django_prometheus.middleware.PrometheusBeforeMiddleware',
-=======
     'django_prometheus.middleware.PrometheusBeforeMiddleware',  # Gardez une seule instance ici
->>>>>>> 9efe092ddad2fe2f3722e48457a65a6b0ad9289b
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -65,27 +53,19 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-<<<<<<< HEAD
     'django_prometheus.middleware.PrometheusBeforeMiddleware',
-]
-=======
  ]
 
->>>>>>> 9efe092ddad2fe2f3722e48457a65a6b0ad9289b
 
 ROOT_URLCONF = 'django_arms.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-<<<<<<< HEAD
-        'DIRS': [],
-=======
         
 
         'DIRS': [os.path.join(BASE_DIR, 'armsApp', 'templates')],
     
->>>>>>> 9efe092ddad2fe2f3722e48457a65a6b0ad9289b
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,10 +93,6 @@ DATABASES = {
 }
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 9efe092ddad2fe2f3722e48457a65a6b0ad9289b
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
